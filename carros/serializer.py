64c3dbf04c9f26cf0ad9b924b8eda78e160705a7
MatheusSaraiva <saraivarego@gmail.com>
2021-10-01 14:02:43 -0300

    Mostrando apenas as colunas necesárias nas listagens

--- carros/serializer.py
+++ carros/serializer.py
@@ -6,6 +6,13 @@ class CarroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carro
         fields = ['nome', 'km_por_galao', 'cilindros', 'cavalor_de_forca', 'peso', 'aceleracao', 'ano', 'origem']
+        extra_kwargs = {
+            "km_por_galao": {'write_only': True},
+            "cilindros": {'write_only': True},
+            "cavalor_de_forca": {'write_only': True},
+            "peso": {'write_only': True},
+            "aceleracao": {'write_only': True},
+        }
 
 
 class MarcaSerializer(serializers.ModelSerializer):
