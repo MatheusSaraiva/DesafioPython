35ac3fc3d87eb139cd17b0c758fafecdf5c2576f
MatheusSaraiva <saraivarego@gmail.com>
2021-10-01 22:47:48 -0300

    adição de documentação Swagger

--- gerenciamento_carros/settings.py
+++ gerenciamento_carros/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'carros',
     'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- gerenciamento_carros/urls.py
+++ gerenciamento_carros/urls.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from carros.views import CarrosViewSet, MarcaViewSet
 from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Gerenciamento de Carros",
+      default_version='v1',
+      description="API de gerenciamento de carros",
+      terms_of_service="#",
+      contact=openapi.Contact(email=""),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register('carros', CarrosViewSet, basename='Carros')
@@ -11,4 +27,5 @@ router.register('marcas', MarcaViewSet, basename='Marcas')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
